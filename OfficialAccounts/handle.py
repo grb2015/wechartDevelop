247cc60a20431b3d902f38ccd1ed7b2a0f4f3973
grb2015 <949409706@qq.com>
2019-10-05 11:41:01 +0000

    官方代码有问题（python2） 修复token认证的bug（python3）
    refer https://blog.csdn.net/qq_39866513/article/details/83218731

--- OfficialAccounts/handle.py
+++ OfficialAccounts/handle.py
@@ -16,11 +16,19 @@ class Handle(object):
             echostr = data.echostr
             token = "awsWechartToken2019" #请按照公众平台官网\基本配置中信息填写
 
-            list = [token, timestamp, nonce]
+            list = [token,timestamp,nonce]
             list.sort()
             sha1 = hashlib.sha1()
-            map(sha1.update, list)
-            hashcode = sha1.hexdigest()
+            sha1.update(list[0].encode("utf-8"))
+            sha1.update(list[1].encode("utf-8"))
+            sha1.update(list[2].encode("utf-8"))
+            hashcode = sha1.hexdigest() #获取加密串
+
+            # list = [token, timestamp, nonce]
+            # list.sort()
+            # sha1 = hashlib.sha1()
+            # map(sha1.update, list)
+            # hashcode = sha1.hexdigest()
             print ("handle/GET func: hashcode, signature: ", hashcode, signature)
             if hashcode == signature:
                 return echostr
