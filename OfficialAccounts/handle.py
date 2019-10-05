74cb5421d4463d49c422df6dd5c2633adba9efd5
grb2015 <949409706@qq.com>
2019-10-05 12:03:47 +0000

    实现“图”尚往来  粉丝发送一张图片，公众号原图发回

--- OfficialAccounts/handle.py
+++ OfficialAccounts/handle.py
@@ -37,21 +37,26 @@ class Handle(object):
                 return ""
         except Exception as Argument:
             return Argument
-
     def POST(self):
-        try:
-            webData = web.data()
-            print ("Handle Post webdata is ", webData)
-   #后台打日志
-            recMsg = receive.parse_xml(webData)
-            if isinstance(recMsg, receive.Msg) and recMsg.MsgType == 'text':
-                toUser = recMsg.FromUserName
-                fromUser = recMsg.ToUserName
-                content = "test"
-                replyMsg = reply.TextMsg(toUser, fromUser, content)
-                return replyMsg.send()
-            else:
-                print ("暂且不处理")
-                return "success"
-        except Exception as Argment:
-            return Argment
+	    try:
+	        webData = web.data()
+	        print ("Handle Post webdata is ", webData )  #后台打日志
+	        recMsg = receive.parse_xml(webData)
+	        if isinstance(recMsg, receive.Msg):
+	            toUser = recMsg.FromUserName
+	            fromUser = recMsg.ToUserName
+	            if recMsg.MsgType == 'text':
+	                content = "test"
+	                replyMsg = reply.TextMsg(toUser, fromUser, content)
+	                return replyMsg.send()
+	            if recMsg.MsgType == 'image':
+	                mediaId = recMsg.MediaId
+	                replyMsg = reply.ImageMsg(toUser, fromUser, mediaId)
+	                return replyMsg.send()
+	            else:
+	                return reply.Msg().send()
+	        else:
+	            print( "暂且不处理")
+	            return reply.Msg().send()
+	    except Exception as Argment:
+	        return Argment
